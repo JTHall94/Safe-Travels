4e80e507f030c3a2a70e7455a898a877b192a1f4
Jacob Hall <jacobhall@Jacobs-MBP.localdomain>
2018-11-26 13:42:32 -0500

    Adds contact index and the ability to edit and delete contacts

--- resources/views/Contacts/edit.blade.php
+++ resources/views/Contacts/edit.blade.php
@@ -7,7 +7,22 @@
       <div class="card">
         <div class="card-header">Edit Contact</div>
         <div class="card-body">
-          <p>This will show a form that displays current contact info in editable fields. There will be a save and a cancel button at the bottom. Simple form. </p>
+          <form id="editContact" action="/contacts/{{ $c->id }}" method="post">
+            @csrf
+            @method('PUT')
+              <div class="form-group">
+                  <label for="new_contact_name" class="mt-2">Name:</label>
+                  <input class="form-control mb-2" type="text" id="new_contact_name" name="new_contact_name" value="{{ old('new_contact_name') ? old('new_contact_name') : $c->name }}" placeholder="Name...">
+
+                  <label for="new_contact_email" class="mt-2">Email:</label>
+                  <input class="form-control mb-2" type="text" id="new_contact_email" name="new_contact_email" value="{{old('new_contact_email') ? old('new_contact_email') : $c->email }}" placeholder="Email...">
+
+                  <label for="new_contact_phone" class="mt-2">Phone Number:</label>
+                  <input class="form-control mb-2" type="text" id="new_contact_phone" name="new_contact_phone" value="{{old('new_contact_phone') ? old('new_contact_phone') : $c->phone }}" placeholder="Phone Number...">
+
+                  <button type="submit" class="btn btn-primary float-right">Update</button>
+
+              </div>
         </div>
       </div>
     </div>
--- resources/views/Contacts/index.blade.php
+++ resources/views/Contacts/index.blade.php
@@ -5,12 +5,47 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">Contact List</div>
+        <div class="card-header text-center">Contact List</div>
         <div class="card-body">
           <p>This is where a list of your contacts will be displayed. Each contact will display it's basic information as well as buttons for edit, delete (and possibly favorite).</p>
           <p>At the top of the list there will also be an option for creating a new contact, which will dropdown a hidden form.</p>
         </div>
       </div>
+
+    @foreach(Auth::user()->contacts as $contact)
+      <div class="card mt-4 mb-4">
+        <div class="card-header text-center">{{$contact->name}}</div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <label for="contactdisplayemail" class="mt-2">Email:</label>
+              <input class="form-control mb-2" type="text" id="contactdisplayemail" name="contactdisplayemail" value="{{$contact->email}}" readonly>
+            </div>
+            <div class="col">
+              <label for="contactdisplayemail" class="mt-2">Phone Number:</label>
+              <input class="form-control mb-2" type="text" id="contactdisplayphone" name="contactdisplayphone" value="{{$contact->phone}}" readonly>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer">
+          <div class="row text-center">
+            <div class="col">
+              <a href="contacts/{{ $contact->id }}/edit" class="btn btn-primary">Edit</a>
+            </div>
+            <div class="col">
+              Favorite
+            </div>
+            <div class="col">
+                <form action="/contacts/{{ $contact->id }}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</i></button>
+                </form>
+            </div>
+        </div>
+      </div>
+    @endforeach
+
     </div>
   </div>
 
