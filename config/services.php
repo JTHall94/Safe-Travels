ba8d209a9a248ab188e066c5dfa8cbdb9c685415
Jacob Hall <jacobhall@Jacobs-MBP.localdomain>
2018-11-30 10:28:14 -0500

    Adds mailgun integration to project with a test route for sending emails (WIP)

--- config/services.php
+++ config/services.php
@@ -15,9 +15,9 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'domain' => env('MAIL_USERNAME'),
+        'secret' => env('MAIL_PASSWORD'),
+        'endpoint' => env('MAIL_HOST'),
     ],
 
     'ses' => [
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,10 @@ Route::put('/profile', 'ProfileController@update')->middleware('auth');
 Route::resource('/alerts', 'AlertsController')->middleware('auth');
 
 Route::resource('/contacts', 'ContactsController')->middleware('auth');
+
+Route::get('send_test_email', function(){
+	Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+	{
+		$message->to('jacobthall94@gmail.com');
+	});
+});
